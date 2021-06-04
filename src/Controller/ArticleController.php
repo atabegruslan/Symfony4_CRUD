85ad305d1f4a5924d7fc86bbff4d9b88e3cfe933
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2021-06-04 23:16:07 +0700

    twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController 
+class ArticleController extends AbstractController
 {
   /**
    * @Route("/", name="article_list")
@@ -14,6 +15,8 @@ class ArticleController
    */
   public function index() 
   {
-    return new Response('<p>Hello</p>');
+    //return new Response('<p>Hello</p>');
+
+    return $this->render('articles/index.html.twig'/*, array('articles' => $articles)*/);
   }
 }
