9e565932dff5597bf05349042e85e2075a6030c1
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2021-06-19 18:54:15 +0700

    doctorine

--- readme.md
+++ readme.md
@@ -4,3 +4,19 @@
 - https://symfony.com/doc/current/doctrine.html
 
 `composer create-project symfony/skeleton projectname`
+
+### Doctrine
+
+```
+composer require doctrine maker
+```
+Do connection string in `.env`
+```
+php bin/console doctrine:database:create
+php bin/console make:entity Article
+```
+Add some fields into `Entity/Article.php`
+```
+php bin/console doctrine:migrations:diff
+php bin/console doctrine:migrations:migrate
+```
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Entity\Article;
+
 class ArticleController extends AbstractController
 {
   /**
@@ -17,11 +19,25 @@ class ArticleController extends AbstractController
   {
     //return new Response('<p>Hello</p>');
 
-    $articles = [
-      'aaa' => 'bbb',
-      'ccc' => 'ddd',
-    ];
+    $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
     return $this->render('articles/index.html.twig', ['articles' => $articles]);
+  }
+
+  /**
+   * @Route("/article/save")
+   */
+  public function save() 
+  {
+    $entityManager = $this->getDoctrine()->getManager();
+
+    $article = new Article();
+    $article->setTitle('Aaa');
+    $article->setBody('Bbb');
+
+    $entityManager->persist($article);
+    $entityManager->flush();
+
+    return new Response('Article created with id: ' . $article->getId());
   }
 }
